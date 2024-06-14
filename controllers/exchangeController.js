18cd31f6848296ab827cdfd4c66068157178ad73
Ferran Casals <ferrancasalsfajula@gmail.com>
2024-06-14 16:00:30 +0200

    upd

--- controllers/exchangeController.js
+++ controllers/exchangeController.js
@@ -8,6 +8,7 @@ class Portfolio {
     this.deposit = [];
     this.withdraw = [];
     this.orders = [];
+    this.history = [];
     this.API_KEY = process.env.BINANCE_API_KEY;
     this.API_SECRET = process.env.BINANCE_SECRET_KEY;
 
@@ -52,22 +53,87 @@ class Portfolio {
 
   async setOrders() {
     try {
-      const oneMonthsAgo = new Date().setMonth(new Date().getMonth() - 1);
-      const startTime = new Date(oneMonthsAgo).getTime();
       const endTime = new Date().getTime();
+      for (let i = 12; i >= 1; i--) {
+        const startTime = new Date(
+          new Date().setMonth(new Date().getMonth() - i)
+        ).getTime();
+        const endTimeMonth = new Date(
+          new Date().setMonth(new Date().getMonth() - (i - 1))
+        ).getTime();
 
-      const allOrders = await this.client.convertTradeHistory(
-        startTime,
-        endTime
-      );
+        const allOrders = await this.client.convertTradeHistory(
+          startTime,
+          endTimeMonth
+        );
 
-      for (let i = 0; i < allOrders.data.list.length; i++) {
-        this.orders.push(allOrders.data.list[i]);
+        for (let j = 0; j < allOrders.data.list.length; j++) {
+          this.orders.push(allOrders.data.list[j]);
+        }
       }
     } catch (error) {
       console.log("Error fetching all pairs:", error);
       throw error;
     }
+  }
+
+  async setHistory() {
+    this.deposit[0].forEach((deposit) => {
+      this.history.push({
+        type: "deposit",
+        id: deposit.id,
+        orderId: null,
+        txId: deposit.txId,
+        fromAmount: null,
+        fromAsset: null,
+        toAsset: deposit.coin,
+        toAmount: deposit.amount,
+        ratio: null,
+        side: null,
+        createdAt: new Date(deposit.insertTime).toISOString(),
+        transactionFee: null,
+      });
+    });
+
+    //Withdraw
+    this.withdraw[0].forEach((withdraw) => {
+      this.history.push({
+        type: "withdraw",
+        id: withdraw.id,
+        orderId: null,
+        txId: withdraw.txId,
+        fromAmount: withdraw.amount,
+        fromAsset: withdraw.coin,
+        toAsset: null,
+        toAmount: null,
+        ratio: null,
+        side: null,
+        createdAt: new Date(withdraw.applyTime).toISOString(),
+        transactionFee: withdraw.transactionFee,
+      });
+    });
+
+    //Orders
+    this.orders.forEach((order) => {
+      this.history.push({
+        type: "order",
+        id: order.id,
+        orderId: null,
+        txId: order.txId,
+        fromAmount: order.fromAmount,
+        fromAsset: order.fromAsset,
+        toAsset: order.toAsset,
+        toAmount: order.toAmount,
+        ratio: order.ratio,
+        side: order.side,
+        createdAt: new Date(order.createTime).toISOString(),
+        transactionFee: null,
+      });
+    });
+
+    this.history.sort((a, b) => {
+      return new Date(a.createdAt) - new Date(b.createdAt);
+    });
   }
 
   build() {
@@ -76,6 +142,7 @@ class Portfolio {
       deposit: this.deposit,
       withdraw: this.withdraw,
       orders: this.orders,
+      history: this.history,
     };
   }
 }
@@ -86,8 +153,11 @@ const getPortfolioBinance = asyncHandler(async (req, res) => {
     await portfolio.setAsset();
     await portfolio.setDeposit();
     await portfolio.setWithdraw();
+
     await portfolio.setOrders();
-    const { assets, deposit, withdraw, orders } = portfolio.build();
+    await portfolio.setHistory();
+
+    const { assets, deposit, withdraw, orders, history } = portfolio.build();
     res.status(200).json({
       success: true,
       data: {
@@ -95,6 +165,7 @@ const getPortfolioBinance = asyncHandler(async (req, res) => {
         deposit,
         withdraw,
         orders,
+        history,
       },
     });
   } catch (error) {
